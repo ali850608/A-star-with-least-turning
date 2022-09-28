08476c2b0260bdb98dcea1a576e3d5971606e08e
Tsung-Yen_Tsai <tsung-yen.tsai@farobottech.com>
2022-09-28 16:21:01 +0800

    using smart pointer

--- src/driver.cpp
+++ src/driver.cpp
@@ -28,13 +28,11 @@ int main(int argc, char * argv[])
   std::optional<VertexDesc> v1 = getVertexFromLabel(graph_, vm["start"].as<std::string>());
   std::optional<VertexDesc> v2 = getVertexFromLabel(graph_, vm["goal"].as<std::string>());
 
-  SpaceTimeAStar * a_star = new SpaceTimeAStar();
+  auto a_star = std::make_shared<SpaceTimeAStar>();
 
   auto path = a_star->findOptimalPath(graph_, *v1, *v2);
   std::cout << path.size() << std::endl;
   for (const auto & v : path) std::cout << getVertexLabel(graph_, v.location) << ";";
-
-  delete a_star;
 
   return 0;
 }
